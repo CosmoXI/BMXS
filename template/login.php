56d3992a0089f8afe99087cf968a9a18925f965f
Dam'z <datitan11@gmail.com>
2017-02-21 19:11:26 +0100

    Add files via upload

--- template/login.php
+++ template/login.php
@@ -21,7 +21,7 @@
 
 <body>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <a class="navbar-brand" href="../index.php">BMXS - Connexion</a>
